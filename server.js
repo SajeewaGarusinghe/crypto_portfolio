cb7dbd11da57b981a69e3ef821731d50ecf9665d
sajeewa garusinghe <sajeeva616@gmail.com>
2023-03-18 15:21:47 +0530

    worker threads added to optimize code,time reduce by half than before

--- server.js
+++ server.js
@@ -1,40 +1,71 @@
-var fs = require('fs');
-var { parse } = require('csv-parse');
+const { Worker } = require('worker_threads');
+const fs = require('fs');
+ 
+// const os = require('os');
+const path = require('path');
 
-var start = new Date();
-
-// const fileName = 'transactions.csv';
-const fileName = './files/sample.csv';
-
+const fileName = 'transactions.csv';
 const balance = {};
+const startTime = new Date();
 
-fs.createReadStream(fileName)
-  .pipe(parse({ delimiter: ',', from_line: 2 }))
-  .on('data', function (row) {
-    let timestamp = row[0];
-    let transaction_type = row[1].toString();
-    let token = row[2].toString();
-    let amount = parseFloat(row[3]);
-    if (balance[token]) {
-      if (transaction_type == 'DEPOSIT') {
-        balance[token]['value'] += amount;
+// const numCPUs = os.cpus().length;
+const numCPUs = 5;
+const upto =30000000;
+const range = parseInt(upto / numCPUs);
+const addEnd = upto % numCPUs;
+
+let start = 2;
+let end = range;
+let temp = 0;
+
+let workerCompleted = 0;
+
+for (let i = 0; i <= numCPUs; i++) {
+
+  
+
+  if (i == 0) {
+    i++;
+  }
+  if (temp == 0) {
+    temp = range;
+  } else if (temp > 0) {
+    start = temp + 1;
+    end = range * i;
+    temp = end;
+  }
+  if (i == numCPUs) {
+    end += addEnd;
+  }
+  // console.log(`start ${start}  end ${end}`);
+
+  const worker = new Worker(path.join(__dirname, 'worker.js'), {
+    workerData: { fileName, fromLine: start,toLine:end },
+  });
+
+  worker.on('message', (result) => {
+    // console.log(result);
+    for (let token in result) {
+      if (balance[token]) {
+        balance[token]['value'] += result[token]['value'];
       } else {
-        balance[token]['value'] -= amount;
-      }
-    } else {
-      if (transaction_type == 'DEPOSIT') {
-        balance[token] = { value: amount };
-      } else {
-        balance[token] = { value: -amount };
+        balance[token] = result[token];
       }
     }
-  })
-  .on('end', function () {
-    console.log(balance);
-    console.log('Process Completed.');
-    var end = new Date() - start;
-    console.log('Execution time: %dms', end);
-  })
-  .on('error', function (error) {
-    console.error(error.message);
   });
+
+  worker.on('error', (err) => {
+    console.error(err);
+  });
+
+  worker.on('exit', () => {
+    workerCompleted++;
+
+    if (workerCompleted === numCPUs) {
+      console.log(balance);
+      // console.log('Process Completed.');
+      const end = new Date() - startTime;
+      console.log('Execution time: %ds', end/1000);
+    }
+  });
+}
